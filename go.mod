5fc3ed4e7d7884508f60e21ba4f3ca142cd8c5e2
kubatovtamas <kubatovtamas01@gmail.com>
2023-11-04 17:11:56 +0100

    Add database connection test

--- go.mod
+++ go.mod
@@ -1,7 +1,12 @@
 // +heroku goVersion 1.16
 
-module fiber
+module secret-santa
 
 go 1.16
 
-require github.com/gofiber/fiber/v2 v2.42.0
+require (
+	github.com/gofiber/fiber/v2 v2.42.0
+	github.com/gofiber/template/html/v2 v2.0.5 // indirect
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/lib/pq v1.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
-
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 )
 
 func getPort() string {
@@ -18,12 +22,31 @@ func getPort() string {
 }
 
 func main() {
-	app := fiber.New()
+	// Connect to the PostgreSQL database using the data source name
+	fmt.Println(os.Getenv("DATABASE_PRIVATE_URL"))
+	fmt.Println(os.Getenv("test"))
+	
+	db, err := sqlx.Connect("postgres", os.Getenv("DATABASE_URL"))
+    if err != nil {
+		log.Fatalln(err)
+	}
+
+	// // Ping the database to ensure it's reachable
+	if err := db.Ping(); err != nil {
+		log.Fatalln("Failed to ping the database:", err)
+	} else {
+		log.Println("Successfully connected to the database.")
+	}
+
+	defer db.Close()
+
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{Views: engine})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"message": "Hello, Railway!",
-		})
+		return c.Render("index",  fiber.Map{
+            "Title": "Hello, World!",
+        })
 	})
 
 	app.Listen(getPort())
